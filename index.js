64e4f2218990223aa4a0921ea3d1d272a3ae7537
Sheila Nery <sheila.sane@gmail.com>
2023-06-14 11:07:25 -0300

    Add cloud

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 
 // Connection URL
-const url = "mongodb://localhost:27017";
+//const url = "mongodb://localhost:27017";
+
+const url = "mongodb+srv://admin:SpO5RvN4FwBwSMSL@cluster0.antlhk0.mongodb.net";
 const client = new MongoClient(url);
 
 // Database Name
@@ -96,7 +98,7 @@ async function main() {
     res.send("Item removido com sucesso!");
   });
 
-  app.listen(3000, function () {
+  app.listen(process.env.PORT || 3000, function () {
     console.log("Aplicação rodando em http://localhost:3000");
   });
 }
